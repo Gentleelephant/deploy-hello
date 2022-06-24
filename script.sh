e0eb74e4ee67a428936e3b16ebdde3a9cbed3ff2
zhangpeng <1132960613@qq.com>
2022-06-24 13:53:52 +0800

    update script

--- script.sh
+++ script.sh
@@ -1,13 +1,16 @@
 #!/usr/bin/env bash
 
+VERSION=v1.0.0
+APPNAME=test-github-action
+
 # build the project
 go build -o ./bin/deploy-hello main.go
 
 # build image
-docker build -t birdhk/deploy-hello:v1 -f Dockerfile .
+docker build -t birdhk/$APPNAME:v1 -f Dockerfile .
 
 # docker login
 docker login --username=birdhk --password=zp521314....
 
 # push image to docker hub
-docker push birdhk/deploy-hello:v1
+docker push birdhk/$APPNAME:$VERSION
