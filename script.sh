c0dcd23f0a2e97fae88dbdae907be4f4927285d0
zhangpeng <1132960613@qq.com>
2022-06-24 13:49:49 +0800

    update script

--- script.sh
+++ script.sh
@@ -4,7 +4,10 @@
 go build -o ./bin/deploy-hello main.go
 
 # build image
-docker build -t deploy-hello:v1 -f Dockerfile .
+docker build -t birdhk/deploy-hello:v1 -f Dockerfile .
+
+# docker login
+docker login --username=birdhk --password=zp521314....
 
 # push image to docker hub
-docker push deploy-hello:v1
+docker push birdhk/deploy-hello:v1
